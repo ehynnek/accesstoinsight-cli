c898452c398004ff8e52350fcae940d200a0b294
ehynnek <ehynnek@gmail.com>
2023-07-12 02:01:42 -0500

    wrote first options for choosing information

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
+import os
 
 welcome_mantra = """The non-doing of any evil,
 the performance of what's skillful,
 the cleansing of one's own mind:
     this is the teaching
     of the Awakened."""
+copy = ['thanissaro bhikku', 1997]
+cname = copy[0]
+cyear = copy[1]
 
 welcome = "Welcome to the unofficial terminal client for Access to Insight; for resources on using this application, " \
           "use the command ati --help.\n\nTo view copyright on a passage, use the command ati --c, which will " \
@@ -14,3 +18,17 @@ welcome = "Welcome to the unofficial terminal client for Access to Insight; for 
 print(welcome)
 print()
 print(welcome_mantra)
+
+choice = input("What would you like to see? Options are: help, copyright, sutta.")
+if choice == 'sutta':
+    sutta_choice = input("Please enter a letter to display a section of the index corresponding to that letter.")
+if choice == 'help':
+    print("options")
+if choice == 'copyright':
+    print("Copyright " + cyear + cname.title())
+else:
+    print(
+        'An input was deteced that was not among those listed by the program; if you wish to exit the program, you can interruprt the process by pressing cntrl+c.')
+
+if sutta_choice == 'a':
+    sutta_a = input("Please select one from the following:")
