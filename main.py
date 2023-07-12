1a8873ba77b0450128f3aff81d477eb2002f3e59
ehynnek <ehynnek@gmail.com>
2023-07-11 21:30:19 -0500

    added more text

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 
-welcome_mantra =  """The non-doing of any evil,
+welcome_mantra = """The non-doing of any evil,
 the performance of what's skillful,
 the cleansing of one's own mind:
     this is the teaching
     of the Awakened."""
 
-print (welcome_mantra)
+welcome = "Welcome to the unofficial terminal client for Access to Insight; for resources on using this application, " \
+          "use the command ati --help.\n\nTo view copyright on a passage, use the command ati --c, which will " \
+          "display copyright information on the last passage you viewed."
+
+# There is likely be a better way to add a line break without the blank print command.
+print(welcome)
+print()
+print(welcome_mantra)
