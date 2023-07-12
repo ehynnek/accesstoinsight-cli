b637088e879f1aed4b8e212acb1027c1f4437599
Elise Hynnek <ehynnek@gmail.com>
2023-07-11 19:21:26 -0500

    initial file upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Press Shift+F10 to execute it or replace it with your code.
+
+welcome_mantra =  """The non-doing of any evil,
+the performance of what's skillful,
+the cleansing of one's own mind:
+    this is the teaching
+    of the Awakened."""
+
+print (welcome_mantra)
