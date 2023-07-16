1c68ec558cc088415f6e92e37dc5e56e49746d42
ehynnek <ehynnek@gmail.com>
2023-07-16 18:43:54 -0500

    bash commit

---
+++ ati-cli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+alias ati = "./main.py"
+echo "Thank you for installing accesstoinsight-cli!"
+echo "\nThe main python program can be launched with the command 'ati'. Further help can be found within the program by typing 'help'."
